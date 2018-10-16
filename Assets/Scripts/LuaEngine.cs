5bb2a926a12fc2b018a76f3acfbfbdcf59e19ded
hongjie <32968210@qq.com>
2018-10-16 16:58:44 +0800

    first commit

---
+++ Assets/Scripts/LuaEngine.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using LuaInterface;
+using UnityEngine;
+/// <summary>
+/// tolua 启动入口，将此脚本动态绑定到一个不销毁的 GameObject 上，通常跟游戏的 GameManagr 绑在同物体上
+/// </summary>
+public class LuaEngine : LuaClient {
+
+    protected override LuaFileUtils InitLoader () {
+        return new LuaResLoader ();
+    }
+
+    /// <summary>
+    /// 可添加或修改搜索 lua 文件的目录
+    /// </summary>
+    protected override void LoadLuaFiles () {
+#if UNITY_EDITOR
+        // 添加编辑器环境下获取 lua 脚本的路径（Assets/Lua）
+        luaState.AddSearchPath (Application.dataPath + "/Lua");
+#endif
+        OnLoadFinished ();
+    }
+}
